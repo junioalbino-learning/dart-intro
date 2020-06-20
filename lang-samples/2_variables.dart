c72c40c649f2824a4351ef85d572b9547324a96f
Junio Albino <junio.albino@localiza.com>
2020-06-20 16:52:19 -0300

    Section 1 to 4

---
+++ lang-samples/2_variables.dart
@@ -0,0 +1,8 @@
+var name = 'Voyager I';
+var year = 1977;
+var antennaDiameter = 3.7;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+var image = {
+  'tags': ['Saturn'],
+  'url': '//path/to/saturn.jpg'
+};
---
+++ lang-samples/3_control_flow_statements.dart
@@ -0,0 +1,22 @@
+var year = 1977;
+var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+main() {
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+}
